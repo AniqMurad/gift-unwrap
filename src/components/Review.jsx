5f8733295e83ebc8e84c9f333c4bba637bad8c17
AniqMurad <nathaaniq0506@gmail.com>
2025-03-25 07:12:48 +0500

    review dynamix

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import flowers from '../assets/revfl.png'
-import Avatar from '../assets/Avatar.png'
-import { StarRating } from './icons'
-
+import React from "react";
+import flowers from "../assets/revfl.png";
+import Avatar from "../assets/Avatar.png";
+import { StarRating } from "./icons";
+import { customerReviews } from "../components/ReviewsData";
 
 const Review = () => {
+  const randomReview = customerReviews[Math.floor(Math.random() * customerReviews.length)];
+
   return (
-    <div className='bg-[#F9F1F0] flex mb-3 p-2 pb-0'>
-      <div className='w-[60%] px-16 py-4'>
-        <p className='text-3xl text-bold mt-5'>
-        "I absolutely love this shop! The products are high-quality and the customer service is excellent. I always leave with exactly what I need and a smile on my face."
-        </p>
-        <div className='w-[50%] p-2 flex gap-2 items-center my-auto'>
-          <img src={Avatar} alt='' />
-          <div>
-            <p className='text-lg font-medium tracking-wide'>LANA RHOADES</p>
-            <StarRating />
+    <div className="bg-[#F9F1F0] flex mb-3 pb-0 items-center">
+      <div className="w-[60%] px-26">
+        <div className="w-[630px] flex flex-col justify-between">
+          <p className="text-[30px] mb-5">"{randomReview.review}"</p>
+          <div className="w-[50%] flex gap-4 items-center">
+            <img src={Avatar} alt="User Avatar" />
+            <div>
+              <p className="text-[18px] font-medium tracking-wide uppercase mb-1">{randomReview.name}</p>
+              <StarRating rating={randomReview.rating} />
+            </div>
           </div>
         </div>
       </div>
-      <div className='w-[40%]'>
-        <img src={flowers} alt='' />
-      </div>
-      
-    </div>
-  )
-}
 
-export default Review
+      <div className="w-[40%] flex justify-center items-center h-full">
+        <img src={flowers} alt="Flowers" />
+      </div>
+    </div>
+  );
+};
+
+export default Review;
