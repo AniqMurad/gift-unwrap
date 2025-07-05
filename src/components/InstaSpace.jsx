e0f1bff9b04a2a6a43d6f7468b2cf79e44c6b220
Shaharyal-beg <118657977+Shaharyal-beg@users.noreply.github.com>
2025-07-05 15:13:49 +0500

    Home page completly responsive

--- src/components/InstaSpace.jsx
+++ src/components/InstaSpace.jsx
@@ -1,12 +1,12 @@
 const InstaSpace = () => {
   return (
-    <div className='my-8 py-1 text-center'>
-      <h1 className='font-bold text-3xl mb-2'>GiftUnwrap On Instagram</h1>
-      <p className='font-normal text-md'>#Anvougetheme</p>
-      <div className='flex bg-[#E9E9E9] my-8 justify-center'>
+    <div className='my-4 sm:my-6 lg:my-8 py-1 text-center px-4 sm:px-6 lg:px-0'>
+      <h1 className='font-bold text-xl sm:text-2xl lg:text-3xl mb-2'>GiftUnwrap On Instagram</h1>
+      <p className='font-normal text-sm sm:text-md'>#Anvougetheme</p>
+      <div className='flex bg-[#E9E9E9] my-4 sm:my-6 lg:my-8 justify-center px-2 sm:px-4 lg:px-0'>
 
         {/* Center - Instagram Widget */}
-        <div className='w-[50%] h-[350px] bg-[#DCDCDC] flex justify-center items-center'>
+        <div className='w-full sm:w-[80%] lg:w-[50%] h-[250px] sm:h-[300px] lg:h-[350px] bg-[#DCDCDC] flex justify-center items-center'>
           {/* LightWidget Instagram Embed */}
           <iframe
             src="//lightwidget.com/widgets/fb6cdf5a8aed578ea0f77754c441adba.html"
